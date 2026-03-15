2ca2e84f9287e6283bc6edb4bb1558c89ee92bc7
Aya <fergani2004aya@gmail.com>
2026-03-16 00:25:14 +0100

    aya

--- data/rules.py
+++ data/rules.py
@@ -39,3 +39,29 @@ def ajouter_regle(line):
         (sid, message, protocol, src_ip, src_port, dst_ip, dst_port, action, line)
     )
     conn.commit()
+
+def modifier_regle(first_sid,line):
+    action = line.split()[0]
+    protocol = line.split()[1]
+    src_ip = line.split()[2]
+    src_port = line.split()[3]
+    dst_ip = line.split()[5]
+    dst_port = line.split()[6]
+
+    msg = re.search(r'msg:"(.*?)"', line)
+    sid = re.search(r'sid:(\d+)', line)
+
+    message = msg.group(1) if msg else ""
+    sid = sid.group(1) if sid else None
+
+    cursor.execute(
+        """
+           UPDATE regles
+           SET sid = %s, message = %s, protocol = %s,
+               src_ip = %s, src_port = %s, dst_ip = %s,
+               dst_port = %s, action = %s, rule = %s
+           WHERE sid = %s
+           """,
+        (sid, message, protocol, src_ip, src_port, dst_ip, dst_port, action, line, first_sid)
+    )
+    conn.commit()
